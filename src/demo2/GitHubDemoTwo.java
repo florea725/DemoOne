a51636120323bcd1fa253658fa1b59a3a65a7445
agg72 <agg72@DESKTOP-BN6NDO2.fios-router.home>
2021-03-21 15:30:14 -0400

    added comment

--- src/demo2/GitHubDemoTwo.java
+++ src/demo2/GitHubDemoTwo.java
@@ -1,5 +1,13 @@
 package demo2;
 
 public class GitHubDemoTwo {
+	public static void main (String [] args) {
+		System.out.println("My first github commit.....");
+		
+		System.out.println("I DID IT");
+	}
+	
+	// you added new code and now you have to share with the team
+	
 
 }
